7b51c03bb038730589ec5f7d8face1863810f8a7
Po <22870592+cover-me@users.noreply.github.com>
2023-05-29 16:22:19 +0800

    Update and rename Keithley6221_20230430.py to Keithley6221_20230529.py

--- qt/qtlab new drivers/Keithley6221_20230529.py
+++ qt/qtlab new drivers/Keithley6221_20230529.py
@@ -7,15 +7,23 @@ import types
 import logging
 # import math
 
-class Keithley6221_20230430(Instrument):
+class Keithley6221_20230529(Instrument):
 
     def __init__(self, name, address):
         logging.debug(__name__ + ' : Initializing instrument')
         Instrument.__init__(self, name, tags=['physical'])
+        print '%-15s\t%-35s\t%-15s'%(name, address, self.__module__)
+        
         self._address = address
         self._visainstrument = visa.instrument(self._address)
         self._visainstrument.clear()
-
+        self._initialize_parameters()
+        self.get_all()
+        
+    def _initialize_parameters(self):
+        # Parameters that are class attributes 
+        self.attribute_parameters = ['_address', '__module__']
+        
         self.dict_parameters = { 
             'ID':
             {
@@ -49,34 +57,58 @@ class Keithley6221_20230430(Instrument):
             },
         }
         
-        self.add_parameter('Address', flags=Instrument.FLAG_GET, type=types.StringType)
+        self._add_parameters()# add attribute_parameters and dict_parameters
         
-        for i in self.dict_parameters:
-            cmd = self.dict_parameters[i]['get_cmd']
+    def _add_parameters(self):
+        '''
+        Add parameters from class attributes and parameter dictionary
+        '''
+        # Add attribute parameters
+        for i in self.attribute_parameters:
+            para_name = i.strip('_').upper()
+            func = lambda attr_name=i: getattr(self,attr_name) 
+            setattr(self, 'do_get_%s'%para_name, func)
+            self.add_parameter(para_name, flags=Instrument.FLAG_GET, type=types.StringType)
+            
+        self._add_parameters_from_dict(self.dict_parameters)
+        
+    def _add_parameters_from_dict(self,para_dict):
+        # Add other parameters
+        for i in para_dict:
+            cmd = para_dict[i]['get_cmd']
             if cmd:
-                func = lambda cmd=cmd: self._query(cmd)
+                func = lambda cmd=cmd,flag=0: self._query(cmd,flag)
                 setattr(self, 'do_get_%s'%i, func)
             
-            cmd = self.dict_parameters[i]['set_cmd']
+            cmd = para_dict[i]['set_cmd']
             if cmd:
                 func = lambda x,cmd=cmd: self._execute(cmd%x)
                 setattr(self, 'do_set_%s'%i, func)
 
-            self.add_parameter(i, **self.dict_parameters[i]['kw'])
+            self.add_parameter(i, **para_dict[i]['kw'])
+            
 
-        self.get_all()
+    def close_session(self):
+        self._visainstrument.close() 
 
     def _execute(self, message):
         return self._visainstrument.write(message)
         
-    def _query(self, message):
-        return self._visainstrument.ask(message)
-
-    def get_all(self):
-        self.get_Address()
-        for i in self.dict_parameters:
-            self.get(i)
+    def _query(self, message, flag=0):
+        '''
+        flag, 0 (default): write command and read respond, 1: write only, 2: read only
+        The reading can be non-atomic with the parameter flag
+        '''
+        if  flag != 2:
+            self._visainstrument.write(message)
+        if flag != 1:
+            ans = self._visainstrument.read()
+            return self._parse(ans,message)
+        return None
         
-
-    def do_get_Address(self):
-        return self._address
+    def _parse(self, ans, message):
+        return ans
+            
+    def get_all(self):
+        for i in self.get_parameters():
+            self.get(i)
