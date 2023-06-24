2ff240061449ef8203bf7f02922845520a4aaddc
Po <22870592+cover-me@users.noreply.github.com>
2023-06-24 20:42:16 +0800

    Update and rename IPS_ethernet_20230529_tritonxl.py to IPS_ethernet_20230624_tritonxl.py

--- qt/qtlab new drivers/IPS_ethernet_20230624_tritonxl.py
+++ qt/qtlab new drivers/IPS_ethernet_20230624_tritonxl.py
@@ -1,29 +1,38 @@
+# https://github.com/cover-me/repository/tree/master/qt/qtlab%20new%20drivers
+
 from instrument import Instrument
-from time import sleep
-import socket
-import msvcrt
+from time import time, sleep
 import visa
 import types
 import logging
-# import math
+import math
+import msvcrt
 
-class IPS_ethernet_20230529_tritonxl(Instrument):
+class IPS_ethernet_20230624_tritonxl(Instrument):
     MARGIN = 1e-3# T
+    # Check the power suppley settings
     # PT1 8.3625 A/T,14 T
-    # TritonXL 8.7399, 16
+    # TritonXL 8.7399 A/T, 16 T
     MAX_FIELD = 16
-    AtoB = 8.7399
     MAX_RATE = 0.2
 
-    def __init__(self, name, address, term_chars = '\n'):
+    def __init__(self, name, address, term_chars = '\n', timeout=2):
         logging.debug(__name__ + ' : Initializing instrument')
-        Instrument.__init__(self, name, tags=['physical'])
+        Instrument.__init__(self, name, tags=['magnet'])
         print '%-15s\t%-35s\t%-15s'%(name, address, self.__module__)
         
         self._address = address
-        self._visainstrument = visa.instrument(self._address, term_chars = term_chars)
+        self._visainstrument = visa.instrument(self._address, term_chars = term_chars, timeout=timeout)
         self._visainstrument.clear()
+        self._initialize_parameters()
+        self.get_all()
+        # qt.flow.connect('measurement-start', self._measurement_start_cb)
+        # qt.flow.connect('measurement-end', self._measurement_end_cb)
 
+    def _initialize_parameters(self):
+        # Parameters that are class attributes 
+        self.attribute_parameters = ['_address', '__module__']
+        
         self.dict_parameters = {
             'ID':
             {
@@ -33,21 +42,19 @@ class IPS_ethernet_20230529_tritonxl(Instrument):
             },
             'margin':
             {
-                'get_cmd':'',
-                'set_cmd':'',
                 'kw':{'type':types.FloatType,'flags':Instrument.FLAG_GETSET,'units':'T'}
             },
             'field': 
             {   
                 'get_cmd':'READ:DEV:GRPZ:PSU:SIG:FLD',
                 'set_cmd':'',
-                'kw':{'type':types.FloatType,'flags':Instrument.FLAG_GETSET,'units':'T', 'minval':-self.MAX_FIELD, 'maxval':self.MAX_FIELD, 'format':'%.5f'}
+                'kw':{'type':types.FloatType,'flags':Instrument.FLAG_GETSET,'units':'T', 'minval':-self.MAX_FIELD, 'maxval':self.MAX_FIELD}
             },
             'field_rate': 
             {   
                 'get_cmd':'READ:DEV:GRPZ:PSU:SIG:RFST',
                 'set_cmd':'SET:DEV:GRPZ:PSU:SIG:RFST:%s',
-                'kw':{'type':types.FloatType,'flags':Instrument.FLAG_GETSET,'units':'T/min', 'minval':0.0, 'maxval':self.MAX_RATE, 'format':'%.5f'}
+                'kw':{'type':types.FloatType,'flags':Instrument.FLAG_GETSET,'units':'T/min', 'minval':0.0, 'maxval':self.MAX_RATE}
             },
             'action': 
             {   
@@ -57,32 +64,50 @@ class IPS_ethernet_20230529_tritonxl(Instrument):
             },
            
         }
-        
-        # Add parameters
-        self.add_parameter('Address', flags=Instrument.FLAG_GET, type=types.StringType)
-        
-        for i in self.dict_parameters:
-            cmd = self.dict_parameters[i]['get_cmd']
-            if cmd:
-                func = lambda cmd=cmd,flag=0: self._query(cmd,flag)
-                setattr(self, 'do_get_%s'%i, func)
-            
-            cmd = self.dict_parameters[i]['set_cmd']
-            if cmd:
-                func = lambda x,cmd=cmd: self._execute(cmd%x)
-                setattr(self, 'do_set_%s'%i, func)
-
-            self.add_parameter(i, **self.dict_parameters[i]['kw'])
-
+        self._add_parameters()# add attribute_parameters and dict_parameters
         self.set_margin(self.MARGIN)
-        self.get_all()
+   
+    def _add_parameters(self):
+        '''
+        Add parameters from class attributes and parameter dictionary
+        '''
+        # Add attribute parameters
+        for i in self.attribute_parameters:
+            para_name = i.strip('_').upper()
+            func = lambda attr_name=i: getattr(self,attr_name) 
+            setattr(self, 'do_get_%s'%para_name, func)
+            self.add_parameter(para_name, flags=Instrument.FLAG_GET, type=types.StringType)
+            
+        self._add_parameters_from_dict(self.dict_parameters)
         
+    def _add_parameters_from_dict(self,para_dict):
+        # Add other parameters
+        for i in para_dict:
+            # A virtual parameter
+            if 'get_cmd' not in para_dict[i] and 'set_cmd' not in para_dict[i]:
+                func = lambda : getattr(self,i)
+                setattr(self, 'do_get_%s'%i, func)
+                
+                func = lambda x: setattr(self,i,x)
+                setattr(self, 'do_set_%s'%i, func)
+                
+                self.add_parameter(i, **para_dict[i]['kw'])
+            # An instrument parameter
+            else:
+                cmd = para_dict[i]['get_cmd']
+                if cmd:
+                    func = lambda cmd=cmd,flag=0: self._query(cmd,flag)
+                    setattr(self, 'do_get_%s'%i, func)
+                
+                cmd = para_dict[i]['set_cmd']
+                if cmd:
+                    func = lambda x,cmd=cmd: self._execute(cmd%x)
+                    setattr(self, 'do_set_%s'%i, func)
+
+                self.add_parameter(i, **para_dict[i]['kw'])
         
     def close_session(self):
         self._visainstrument.close()
-        
-    def do_get_Address(self):
-        return self._address
         
     def _execute(self, message):
         return self._visainstrument.ask(message)
@@ -111,8 +136,7 @@ class IPS_ethernet_20230529_tritonxl(Instrument):
         return None
         
     def get_all(self):
-        self.get_Address()
-        for i in self.dict_parameters:
+        for i in self.get_parameters():
             self.get(i)
             
     def do_set_field(self,val,wait=True):
@@ -120,7 +144,7 @@ class IPS_ethernet_20230529_tritonxl(Instrument):
         self.set_action('RTOS')# system would reset to "HOLD" once field reached
         if wait:
             try:
-                while abs(val - self.get_field()) > self.MARGIN or self.get_action() != 'HOLD':
+                while abs(val - self.get_field()) > self.MARGIN:# or self.get_action() != 'HOLD':
                     self._do_emit_changed()# update the GUI
                     self._check_last_pressed_key()
                     sleep(0.050)
@@ -129,15 +153,15 @@ class IPS_ethernet_20230529_tritonxl(Instrument):
                 raise KeyboardInterrupt
         return True
 
-    def do_set_margin(self, x):
-        self.margin = x
-        
-    def do_get_margin(self):
-        return self.margin
-        
     def _check_last_pressed_key(self):
         last_key = ''
         while msvcrt.kbhit():
            last_key = msvcrt.getch()
         if last_key == '\x05':#ctrl+e(xit)
             raise KeyboardInterrupt
+
+    # def _measurement_start_cb(self, sender):
+        # pass
+
+    # def _measurement_end_cb(self, sender):
+        # pass
